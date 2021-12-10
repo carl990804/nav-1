7dbf6ee42b04c63199fc09c2149a6c59c173b98b
carl990804 <2532848142@qq.com>
2021-12-10 21:07:47 +0800

    init

--- main.js
+++ main.js
@@ -75,11 +75,11 @@ $('.addButton')
         const string=JSON.stringify(hashMap)
         localStorage.setItem('x',string)
     }
-    // $(document).on('keypress',(e)=>{
-    //     const {key}=e
-    //     for(let i=0;i<=hashMap.length;i++){
-    //         if(hashMap[i].logo.toLowerCase()===key){
-    //             window.open(hashMap[i].url)
-    //         }
-    //     }
-    // })
+    $(document).on('keypress',(e)=>{
+        const {key}=e
+        for(let i=0;i<=hashMap.length;i++){
+            if(hashMap[i].logo.toLowerCase()===key){
+                window.open(hashMap[i].url)
+            }
+        }
+    })
